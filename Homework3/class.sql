4f0dd7fe035b94fe798bbcd24e03386fe81777c5
Yuriy Krasnov <118444989+Yuriy8585@users.noreply.github.com>
2023-03-24 21:51:01 +0100

    task

---
+++ Homework3/class.sql
@@ -0,0 +1,9 @@
+CREATE TABLE students (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(255) NOT NULL,
+age INT NOT NULL,
+address VARCHAR(255) NOT NULL
+);
+
+INSERT INTO students (id, name, age, address) VALUES (001, 'Иванов Иван', 20, 'ул. Ленина, д. 10');
+INSERT INTO students (id, name, age, address) VALUES (002, 'Петров Петр', 21, 'ул. Гагарина, д. 5');
